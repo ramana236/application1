8317e17405cba9711f67081b0b318e46c44875b7
ramana236 <rkumar.stvl@gmail.com>
2021-04-02 09:25:33 +0400

    Add files via upload

---
+++ buildImage.sh
@@ -0,0 +1,9 @@
+#This script will build docker image
+
+version=$BUILD_ID
+docker build -t apprepo:$version .
+aws ecr get-login-password --region ap-south-1 --profile ramanapersonal | docker login --username AWS --password-stdin 202714190885.dkr.ecr.ap-south-1.amazonaws.com
+docker tag apprepo:$version 202714190885.dkr.ecr.ap-south-1.amazonaws.com/apprepo:$version
+docker push 202714190885.dkr.ecr.ap-south-1.amazonaws.com/apprepo:$version
+
+echo "Image Successfully pushed to ecr"
