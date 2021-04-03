5f34355e4a491201d954d13dd6afa573ba81a80a
ramana236 <rkumar.stvl@gmail.com>
2021-04-03 16:15:14 +0400

    Update buildImage.sh

--- buildImage.sh
+++ buildImage.sh
@@ -1,7 +1,6 @@
 #This script will build docker image
 
 version=$BUILD_ID
-sudo su -
 docker build -t apprepo:$version .
 aws ecr get-login-password --region ap-south-1  | docker login --username AWS --password-stdin 202714190885.dkr.ecr.ap-south-1.amazonaws.com
 docker tag apprepo:$version 202714190885.dkr.ecr.ap-south-1.amazonaws.com/apprepo:$version
